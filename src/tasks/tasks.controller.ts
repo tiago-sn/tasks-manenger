c1b9de0b954172ce2d72ecf231b9958db1fadea1
Tiago Santos <tiagosn03@gmail.com>
2020-01-15 21:34:27 -0300

    Give Ubuntu permition to write on any file in the project.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Controller } from "@nestjs/common";
+import { TasksService } from "./tasks.service";
 
-@Controller('tasks')
-export class TasksController {}
+@Controller("tasks")
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+}
