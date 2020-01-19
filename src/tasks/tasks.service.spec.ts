a82d7f73f9e3b4b1cbddbb9ab3c9dcf4f4d19745
Tiago Santos <tiagosn03@gmail.com>
2020-01-19 15:07:07 -0300

    task.service: add test..

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { TaskStatus } from './task.model';
 
 describe('TasksService', () => {
   let service: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [TasksService]
     }).compile();
 
     service = module.get<TasksService>(TasksService);
@@ -14,5 +15,18 @@ describe('TasksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('setTaskStatus', () => {
+    it('Update task status', () => {
+      const task = service.createTask({
+        title: 'test title',
+        description: 'test description'
+      });
+
+      const updatedTask = service.setTaskStatus(task.id, TaskStatus.DONE);
+
+      expect(updatedTask.status).toBe(TaskStatus.DONE);
+    });
   });
 });
