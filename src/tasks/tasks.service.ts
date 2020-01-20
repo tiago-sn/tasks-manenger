fdd99be4ed36ae65f32d88b2f1fe70d4c9697aca
Tiago Santos <tiagosn03@gmail.com>
2020-01-19 22:13:02 -0300

    Create CRUD operations to /tasks and point.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from "@nestjs/common";
-import { Task } from "./task.model";
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import uuid from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +10,66 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilter(filterDto: GetTasksFilterDto) {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter(task => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter(
+        task => task.title.includes(search) || task.description.includes(search)
+      );
+    }
+
+    return tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  deleteTask(id: string): Task[] {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+
+    return this.tasks;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
+  setTaskStatus(id: string, status: TaskStatus): Task {
+    let updatedTask: Task;
+
+    this.tasks.forEach(task => {
+      if (task.id === id) {
+        task.status = status;
+        updatedTask = task;
+      }
+    });
+
+    return updatedTask;
   }
 }
