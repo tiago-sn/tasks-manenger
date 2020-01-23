150b21ed3b23bd2391545ec5490e47a067d29028
Tiago Santos <tiagosn03@gmail.com>
2020-01-23 12:05:36 -0300

    Add auto module, controller and service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
